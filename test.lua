4d3808fabaee26cd0caeac0496f363f3331a233c
Devon Palma <deedpa1112@gmail.com>
2024-11-03 18:43:52 -0700

    Update test.lua
    Minor mods to remove some old useless parts of code

--- test.lua
+++ test.lua
@@ -1,111 +1,272 @@
-local function test()
-    -- Load the lowerclass module
-    local lowerclass = require('lib.LowerClass')
+-- -------------------------------------------------------------------------- --
+--                                Customization                               --
+-- -------------------------------------------------------------------------- --
 
-    -- Create a mixin with shared methods
-    ---@class MyMixin
-    local MyMixin = {
-        mixinVar = "This is a mixin variable",
-        greet = function(self)
-            print("Hello from Mixin!")
-        end,
-        included = function(self, class)
-            print(class.name .. " has included MyMixin.")
-        end,
-    }
+--- If true, allows calling lowerclass() to create a new class
+--- Cleaner, and can somewhat be reliable with EmmyLua annotations
+local lowerclass_enableCallMetamethod = true
 
-    -- -------------------------------------------------------------------------- --
-    --                                Basic Class                                 --
-    -- -------------------------------------------------------------------------- --
+--- If true, allows calling lowerclass:new() to create a new class
+--- Very reliable with EmmyLua annotations, but not as clean
+local lowerclass_enableNewMethod = true
 
-    -- Create a basic class with an initializer
-    ---@class Animal : Class
-    local Animal = lowerclass("Animal")
+--- If true, allows calling myClass() to create an instance of a class
+--- Cleaner, but very unreliable with EmmyLua annotations
+---     If used, recommended to use the --[[@as <Class>]] annotation
+local class_enableCallMetamethod = true
 
-    -- Define __init, a variable, and a method in the Animal class
-    function Animal:__init(name)
-        self.name = name
+--- If true, allows calling myClass:new() to create an instance of a class
+--- Very reliable with EmmyLua annotations, but not as clean
+local class_enableNewMethod = true
+
+-- -------------------------------------------------------------------------- --
+--                                 Class Setup                                --
+-- -------------------------------------------------------------------------- --
+
+-- Define LowerClass with local weak tables for method storage
+---@class lowerclass
+---@overload fun(name: string, ...): Class|function
+local lowerclass = {}
+
+-- -------------------------------------------------------------------------- --
+--                                    Core                                    --
+-- -------------------------------------------------------------------------- --
+
+-- Weak tables for storing method data
+local classData = setmetatable({}, { __mode = "k" }) -- Stores class data
+
+-- -------------------------------------------------------------------------- --
+--                         Class Variable Declaration                         --
+-- -------------------------------------------------------------------------- --
+
+--- A very fancy way to create a better __index for a class
+--- @param aClass Class
+--- @param var table|function
+--- @return table|function
+local function __createIndexWrapper(aClass, var)
+    if var == nil then
+        return classData[aClass].lookupDict[name]
+    elseif type(var) == "function" then
+        return function(self, name)
+            return classData[aClass].lookupDict[name] or var(self, name)
+        end
+    else -- if  type(f) == "table" then
+        return function(self, name)
+            return classData[aClass].lookupDict[name] or var[name]
+        end
     end
+end
 
-    Animal.species = "Generic Animal"
-    function Animal:speak()
-        print("Animal sound!")
+--- Ensures a variable is propegated to all subclasses
+--- @param aClass Class
+--- @param name string
+--- @param var any
+local function __propegateClassVariable(aClass, name, var)
+    var = name == "__index" and __createIndexWrapper(aClass, var) or var
+    classData[aClass].lookupDict[name] = var
+
+    for _, child in ipairs(classData[aClass].heirarchyData.children) do
+        if classData[child].definedVariables[name] == nil then
+            __propegateClassVariable(child, name, var)
+        end
     end
+end
 
-    -- Create an instance of Animal and test it
-    local animalInstance = Animal:new("Buddy")
-    print("Name:", animalInstance.name)       -- Output: Buddy
-    print("Species:", animalInstance.species) -- Output: Generic Animal
-    animalInstance:speak()                    -- Output: Animal sound!
+-- Adds / Removes a variable from a class
+---@param aClass Class
+---@param name string
+---@param var any
+local function __declareClassVariable(aClass, name, var)
+    -- print("Declared " .. tostring(aClass) .. "." .. tostring(name) .. " as " .. tostring(var))
+    -- Set the var internally first
+    local dat = classData[aClass]
+    dat.definedVariables[name] = var
 
-    print()
-    -- -------------------------------------------------------------------------- --
-    --                             Class with Mixin                               --
-    -- -------------------------------------------------------------------------- --
-
-    -- Create a new class and include MyMixin
-    ---@class Dog : Class, MyMixin
-    local Dog = lowerclass("Dog")
-    Dog:include(MyMixin)
-
-    -- Add specific properties, an initializer, and methods to Dog
-    function Dog:__init(name, breed)
-        self.name = name
-        self.breed = breed
-    end
-
-    Dog.species = "Dog"
-    function Dog:speak()
-        print("Woof! Woof!")
-    end
-
-    -- Create an instance of Dog and test it
-    local dogInstance = Dog:new("Rover", "Labrador")
-    print("Name:", dogInstance.name)   -- Output: Rover
-    print("Breed:", dogInstance.breed) -- Output: Labrador
-    dogInstance:speak()                -- Output: Woof! Woof!
-    dogInstance:greet()                -- Output: Hello from Mixin!
-
-    print()
-    print()
-
-    -- -------------------------------------------------------------------------- --
-    --                             Class with Metamethods                          --
-    -- -------------------------------------------------------------------------- --
-
-    -- Define a Point class with `__index` and `__add` metamethods
-    ---@class Point : Class
-    ---@operator add: Point
-    local Point = lowerclass("Point")
-
-    -- Add initializer for Point
-    function Point:__init(x, y)
-        self.x = x or 0
-        self.y = y or 0
-    end
-
-    -- Define a default value for z using __index
-    function Point.__index(tbl, key)
-        if key == "z" then
-            return 0 -- default value for z
+    if var == nil then
+        for _, parent in ipairs(dat.heirarchyData.parents) do
+            if parent[name] ~= nil then
+                var = parent
+                break
+            end
         end
     end
 
-    -- Define an __add metamethod for Point
-    function Point.__add(a, b)
-        return Point:new(a.x + b.x, a.y + b.y)
-    end
-
-    -- Create two Point instances
-    local point1 = Point:new(1, 2)
-    local point2 = Point:new(3, 4)
-
-    -- Access the default value for z
-    print("Point1 Z:", point1.z) -- Output: 0
-
-    -- Add points using the __add metamethod
-    local resultPoint = point1 + point2
-    print("Resulting Point:", resultPoint.x, resultPoint.y) -- Output: 4, 6
+    __propegateClassVariable(aClass, name, var)
 end
 
-return test
+-- -------------------------------------------------------------------------- --
+--                            Inheritance / Mixins                            --
+-- -------------------------------------------------------------------------- --
+
+-- Adds a parent to a class
+---@param aClass Class
+---@param parent Class
+local function __addParent(aClass, parent)
+    print("Adding parent " .. tostring(parent) .. " to " .. tostring(aClass))
+
+    local dat = classData[aClass]
+    local parentData = classData[parent]
+
+    table.insert(dat.heirarchyData.parents, parent)
+    table.insert(parentData.heirarchyData.children, aClass)
+
+    for key, value in pairs(parentData.definedVariables) do
+        if not (key == "__index" and type(f) == "table") then
+            __propegateClassVariable(aClass, key, value)
+        end
+    end
+end
+
+-- Checks if the passed object is an instance of a class
+---@param self Class object to check
+---@param aClass Class class to check against
+---@return boolean
+local function __is(self, aClass)
+    -- If instance, extract class
+    if self.class then
+        self = self.class
+    end
+
+    if self == aClass then
+        return true
+    end
+
+    local dat = classData[self]
+    for _, parent in ipairs(dat.heirarchyData.parents) do
+        if __is(parent, aClass) then
+            return true
+        end
+    end
+
+    return false
+end
+
+-- Adds a mixin to a class
+---@param aClass Class
+---@param mixin table
+local function __addMixin(aClass, mixin)
+    for name, method in pairs(mixin) do
+        if name ~= "included" then
+            aClass[name] = method
+        end
+    end
+
+    if type(mixin.included) == "function" then
+        mixin:included(aClass)
+    end
+end
+
+-- -------------------------------------------------------------------------- --
+--                               Table Creation                               --
+-- -------------------------------------------------------------------------- --
+
+--- Creates an instance of a class
+--- @generic T
+--- @param aClass T
+--- @return T
+local function __newInstance(aClass, ...)
+    local classDat = classData[aClass]
+    local instance = { class = aClass }
+
+    setmetatable(instance, classDat.lookupDict)
+
+    if type(instance.__init) == "function" then
+        instance:__init(...)
+    end
+
+    return instance
+end
+
+--- Creates a new class
+--- @param name string
+--- @return Class
+local function __createClass(name, ...)
+    local lookupDict = {}
+    lookupDict.__index = lookupDict
+
+    -- Generate class object
+
+    ---@class Class
+    local aClass = {
+        name = name,
+        include = function(self, mixin)
+            if type(mixin) ~= "table" then
+                error("mixin must be a table")
+            end
+
+            if classData[mixin] == nil then
+                __addMixin(self, mixin)
+            else
+                __addParent(self, mixin)
+            end
+        end,
+    }
+    -- Setup new method if desired
+    if (class_enableNewMethod) then
+        aClass.new = __newInstance
+    end
+
+    -- Generate internal class data
+
+    classData[aClass] = {
+        definedVariables = {},
+        lookupDict = lookupDict,
+        heirarchyData = {
+            children = {},
+            parents = {},
+        }
+    }
+
+    -- Set up metatable for class
+
+    local classMeta = {
+        __index = lookupDict,
+        __tostring = function()
+            return "class(\"" .. name .. "\")"
+        end,
+        __newindex = function(_, key, value)
+            __declareClassVariable(aClass, key, value)
+        end
+    }
+    -- Setup call metamethod if desired
+    if (class_enableCallMetamethod) then
+        classMeta.__call = __newInstance
+    end
+
+    setmetatable(aClass, classMeta)
+
+    -- add "is" method (Added here to ensure its propegated properly)
+    aClass.is = __is
+
+    -- Add all mixins
+    for _, parent in ipairs({ ... }) do
+        aClass:include(parent)
+    end
+
+    return aClass
+end
+
+-- -------------------------------------------------------------------------- --
+--                              LowerClass Setup                              --
+-- -------------------------------------------------------------------------- --
+
+--- Generate metatable for lowerclass
+local lowerclass_mt = {}
+---@diagnostic disable-next-line: param-type-mismatch
+setmetatable(lowerclass, lowerclass_mt)
+
+-- Setup call metamethod if desired
+if (lowerclass_enableCallMetamethod) then
+    lowerclass_mt.__call = function(self, name, ...)
+        return __createClass(name, ...)
+    end
+end
+
+-- Setup new method if desired
+if (lowerclass_enableNewMethod) then
+    lowerclass.new = function(self, name, ...)
+        return __createClass(name, ...)
+    end
+end
+
+return lowerclass
